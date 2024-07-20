31e95eb5c1a0e1b73d5f3f27d165bc894e1894ba
sahilyadav2004 <sahillawaniya4@gmail.com>
2024-07-20 13:48:40 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./components/footer/footer"
+import Hero from "./components/herosection/Hero"
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <Hero/>
+     <Footer/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr></hr>
+      <div className='upper'>
+      <h1>FOLLOW US</h1>
+        <div className='logos'>
+       <a href='https://www.instagram.com/icell.nitkkr?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='><img src='insta.jpeg'/></a> 
+       <a href='https://www.linkedin.com/company/icell-nitkkr/'> <img src='linkdin.jpeg'/></a>
+        <img src='twitter.jpeg'/>
+        </div>
+      </div>
+        
+        <div className='end'>
+        <div className='info'>
+            <h3>INNOVATION CELL</h3>
+            <h3>NIT KURUKSHETRA</h3>
+            <h3>ICELL@NIT.AC.IN</h3>
+            <h3>7418529630</h3>
+        </div>
+        <div className='logo'>
+          <img src='logo.jpeg'></img>
+        </div>
+        </div>
+        <p className='text'>Made with ❤ by Innovation Cell</p>
+    </div>
+  )
+}
+
+export default Footer
